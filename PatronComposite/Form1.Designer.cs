addbef1bba00a8ee1f9b375b3c1fbc1b12d02e76
Chiara Di Giannantonio <77933300+chiaradigii@users.noreply.github.com>
2023-04-30 11:00:53 -0300

    Adding Composite Pattern

---
+++ PatronComposite/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace PatronComposite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "001,1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(45, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 49);
+            this.button2.TabIndex = 1;
+            this.button2.Tag = "002,2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(45, 175);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 49);
+            this.button3.TabIndex = 2;
+            this.button3.Tag = "003,3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(45, 230);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 49);
+            this.button4.TabIndex = 3;
+            this.button4.Tag = "004,4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(198, 68);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Perfil 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(198, 119);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 17);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Perfil 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(45, 285);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(97, 49);
+            this.button5.TabIndex = 6;
+            this.button5.Tag = "005,5";
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ PatronComposite/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PatronComposite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        PermisoSimple ps1,ps2,ps3,ps4,ps5;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se ejecutó la funcoion 1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se ejecutó la funcoion 2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se ejecutó la funcoion 3");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se ejecutó la funcoion 4");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se ejecutó la funcoion 5");
+        }
+
+        private void Mostrar()
+        {
+            //Es la que define que se muestra en la interfaz segun el perfil del usuario
+            int _y = 20;//la que uso para el espacio como acomodo los botones
+            List<Button> _b = new List<Button>();
+            foreach (Control c in this.Controls)//recorro coleccion de controles del form
+            {
+                if(c is Button)//pregunto si es boton
+                {
+                    (c as Button).Visible = false;//Lo primero que hago es ocultar los botones
+                    if (_u.Perfil.Validar(c.Tag.ToString().Split(',')[0]))//si ese usuario en ese perfil valida,
+                    {   //TENGO un tag en cada boton en sus propiedades, ej: boton 1 tiene 001,1
+                        //por eso separa por la coma y solo se queda con la primer parte del tag 001
+                        //ese 001 ese el codigo, ese codigo se usa como parametro en Validar porque validar
+                        ////es buscar si ese codigo existe como codigo dentro del perfil
+                        _b.Add(c as Button); //si tiene el permiso agrega el boton para que el usuario si pueda ver
+                    }
+                }
+            }
+            _b.Sort(new Orden()); //ordeno la lista de botones con SOrt y le mando una instancia de orden
+            foreach (Control c in _b)
+            {
+                c.Visible = true;
+                c.Top = _y; // lo pone en la posicion correspondiente ya que si oculto alguno quedan en cualquier lado del form. Los     
+                _y += 55; 
+                
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            _u.Perfil = _p01; Mostrar();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            _u.Perfil = _p02; Mostrar();
+        }
+
+        PermisoCompuesto pc1, pc2, pc3;
+        Usuario _u;
+        Perfil _p01, _p02;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Tres situaciones
+            //A) Compuestos que son solo perfiles
+            //B) Compuestos que son solo intermedios
+            //C) COmpuestos que pueden ser intermedios de otros compuestos y perfiles
+            
+            _u = new Usuario() { Nome = "Chiara", Apellido= "Digi"};
+
+            ps1 = new PermisoSimple("001");
+            ps2 = new PermisoSimple("002");
+            ps3 = new PermisoSimple("003");
+            ps4 = new PermisoSimple("004");
+            ps5 = new PermisoSimple("005");
+
+            pc1 = new PermisoCompuesto("C01");
+            pc2 = new PermisoCompuesto("C02");
+            pc3 = new PermisoCompuesto("C03");
+
+            pc1.AgregarPermiso(ps1);
+            pc1.AgregarPermiso(ps2);
+            pc1.AgregarPermiso(ps3);
+            pc1.AgregarPermiso(ps4);
+
+            pc2.AgregarPermiso(pc1); 
+            pc2.AgregarPermiso(ps5);
+
+            pc3.AgregarPermiso(ps1);
+            pc3.AgregarPermiso(ps4);
+            pc3.AgregarPermiso(ps5);
+
+            _p01 = new Perfil(pc2);
+            _p02= new Perfil(pc3);
+
+            _u.Perfil = _p01;
+            Mostrar();
+        }
+    }
+
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Apellido { get; set; }
+        public Perfil Perfil { get; set; }
+    }
+    public class Perfil
+    {
+        PermisoCompuesto _pc;//perfil de alguna manera va a estar representado x un permiso compuesto
+        public Perfil(PermisoCompuesto pCompuesto) 
+        { 
+            _pc = pCompuesto; 
+         }
+
+        public bool Validar(string pCodigo)
+        {
+            //a este peril le pasan un codigo ( a donde el perfl quiere entrar)
+            //y le devuelve si ese codigo esta o no permitido
+            return _pc.RetornaPermiso().Exists(x => x.Codigo == pCodigo);
+            //para cada x que retorna RetornaPermiso (lista de permisos simples) se fija 
+            //si hay alguno que coincida (retorna verdadewr)
+        }
+
+    }
+    public abstract class Permiso
+    {
+        //Clase abstracta que sería lo que el patrón plantea como Component
+        
+        string _codigo;
+
+        public Permiso(string pCodigo)
+        {
+            //en este caso los permisos estan emulados por codigo
+            _codigo = pCodigo;
+        }
+        public string Codigo
+        {
+            //propiedad de lectura y escritura para leer o escribir ese código
+            get { return _codigo; }
+            set { _codigo = value; }
+        }
+        //public bool SumaResta {get; set;{
+
+        public abstract List<Permiso> RetornaPermiso();//lista de permisos que es un retorno de una funcion RetornaPermisos que va
+                                                         //a implementar todas las sub clases (por eso es abstracta
+        //Aca podria estar tambien lo de agregar y modificar
+    }
+    public class PermisoSimple : Permiso
+    {
+        public PermisoSimple(String pCodigo): base(pCodigo)//usa constructor de la super clase
+        {
+
+        }
+        public override List<Permiso> RetornaPermiso()
+        {
+            return new List<Permiso>() { this };//este retorna una lista a la cual se agrega al mismo
+            //no me queda otra que sea una lista por la restricción del polimorfismo que hereda de la super clase
+        }
+    }
+    public class PermisoCompuesto : Permiso
+    {
+        List<Permiso> _l; //esta es la lista propia del patron, la que tiene
+                            //los permisos que componen el permiso compuesto
+
+        List<Permiso> _laux;
+
+        public PermisoCompuesto(String pCodigo) : base(pCodigo)
+        {
+            //ademas de heredar constructor de clase base instancia lista de permisos
+            _l = new List<Permiso>();
+        }
+        public void AgregarPermiso(Permiso Ppermiso)
+        {
+            _l.Add(Ppermiso);
+        }
+       public List<Permiso> Retornarcomponentes()
+        {
+            return _l;//este metodo es por algo funcional, no del patron
+        }
+        
+        public override List<Permiso> RetornaPermiso()
+        {
+            //Aca esta la lógica propia del patron composite
+            //esta recursiva
+            _laux = new List<Permiso>();//instancio aca la lista auxiliar
+            RecursivaRetornaPermisos(_l);//llamo a la recursiva y paso la lista original
+            //esta recursiva lo que va a hcaer es cargarme la lista auxiar en sus componentes simples
+            //y para que el metodo la retorne despes
+            return _laux;
+        }
+        private void RecursivaRetornaPermisos(List<Permiso> pLista)
+        {
+            foreach( Permiso p in pLista)//iterator para cada permiso de la lista pregunta si es simpe
+            {
+                if(p is PermisoSimple)
+                {
+                    _laux.Add(p);//si es simple directamente lo agrega a la lista auxiliar
+                }
+                else
+                {
+                    //si es compuesto llama a la recursiva para que lo descomponga
+                    RecursivaRetornaPermisos((p as PermisoCompuesto).Retornarcomponentes());
+                                            //le digo a p q es permiso compuesto (lo casteo) y uso su retornacomponentes que da a lista l (original)
+                }
+            }
+        }
+    }
+    public class Orden : IComparer<Button> //interfaz de comparación
+    {
+        public int Compare(Button x, Button y)//ventaja deesta interfaz es que yo decido el criterio de ordenamiento
+        {
+            //recibe dos objetos Button y tiene que devolver un valor < 0, 0, >0
+            //si es menor q cero significa que el objeto x va a la izquierda del y,
+            //si el valor es 0 significa que ambos objetos tienen la misma prioridad de ordenamiento
+            //si el valor es mayor que 0 significa que y va antes que x
+            int rdo = 0;
+            if (int.Parse(x.Tag.ToString().Split(',')[1]) < int.Parse(y.Tag.ToString().Split(',')[1]))
+            {
+                //Con x.Tag.ToString().Split(',')[1] tomo el segundo elemento del vector tag que me dice la poscicion del boton.
+                rdo = -1;
+
+            }
+            if (int.Parse(x.Tag.ToString().Split(',')[1]) > int.Parse(y.Tag.ToString().Split(',')[1]))
+            {
+                rdo = 1;
+            }
+            return rdo;
+
+
+        }
+    }
+}
